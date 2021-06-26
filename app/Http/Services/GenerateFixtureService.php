cfe7de75495b23e2d796e8518633efec7ef0f06d
Daniel <danielheinze96@gmail.com>
2021-06-26 19:30:27 +0200

    test

--- app/Http/Services/GenerateFixtureService.php
+++ app/Http/Services/GenerateFixtureService.php
@@ -3,24 +3,49 @@
 namespace App\Http\Services;
 
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 
 class GenerateFixtureService
 {
-    public function __construct(array $teams)
-    {
-        $totalTeams = 10;
-        $maxTrials = 10_000_000;
-        $start = time();
 
-        $this->totalTeams = $totalTeams;
-        $this->maxTrials  = $maxTrials;
-        $this->start      = $start;
-        $this->teams      = $teams;
-        $this->fixtures = $this->generateFixtures($teams);
+    public static function generateMeetingsWithSchedule(array $teams, array $details): array
+    {
+        $schedule = [];
+        $fixtures = self::generateMeetings($teams);
+
+        $competitionId = $details['competitionId'];
+        $date = Carbon::createFromDate($details['startDate']);
+        $hour          = $details['firstGameHour'];
+        $lastGameHour  = $details['lastGameHour'];
+        $gameDuration  = $details['gameDuration'];
+        $pitch = 1;
+        $stage = 1;
+
+        foreach ($fixtures as $round => $pair) {
+
+            $round = [];
+            foreach ($pair as $team) {
+
+                $host    = $team[0];
+                $visitor = $team[1];
+
+                $game = [
+                    'host'    => $host,
+                    'visitor' => $visitor,
+                    'date'    => $date,
+                    'time'    => $hour,
+                    'pitch'   => $pitch,
+                    'stage'   => $stage,
+                ];
+
+                $round[] = $game;
+            }
+            $schedule[] = $round;
+        }
+
+        return $schedule;
     }
 
-    private function meetings(array $teams): array
+    private static function generateMeetings(array $teams): array
     {
         $pairs = [];
 
@@ -69,119 +94,30 @@ class GenerateFixtureService
 
         return $result;
     }
-
-    public static function generateSchedule($fixtures)
-    {
-        $request['startDate'] = '2021-01-01';
-        $request['startTime'] = '20:00';
-        $request['endTime']   = '22:00';
-        $request['numOfPitches']   = 2;
-
-        $startDate = $request['startDate'];
-        $startDate = Carbon::createFromDate($startDate);
-
-        $startTime = $request['startTime'];
-        $startTime = Carbon::createFromTimeString($request['startTime']);
-
-        $beginTime = $request['startTime'];
-        $beginTime = Carbon::createFromTimeString($request['startTime']);
-
-        $endTime = $request['endTime'];
-        $endTime = Carbon::createFromTimeString($request['endTime']);
-
-        $date = $startDate;
-        $time = $startTime;
-
-        $gameTime = 90;
-        $pitch = 1;
-
-        foreach ($fixtures as $round => $pair) {
-
-            if ($request['numOfPitches'] >= $pitch) {
-                $pair['pitch'] = $pitch;
-                $pitch++;
-            } else {
-                $pitch = 1;
-                $time->addMinutes($gameTime);
-            }
-            $pair['time']  =  $time;
-
-            if ($request['endTime'] >= $time) {
-                $pair['date']  =  $date;
-            } else {
-                $time = $startTime;
-                $date->addDays(7);
-            }
-        }
-    }
-
-    private function generateFixtures(array $teams)
-    {
-        $trial = 0;
-        $fixtures = [];
-
-        do {
-            if ($trial === $this->maxTrials) {
-                exit('Nie udało się, za dużo prób :(');
-            }
-
-            $trial += 1;
-            $meetings = $this->meetings($teams);
-            $continue = count(array_count_values(array_map('count', $meetings))) !== 1;
-        } while ($continue);
-
-        $meetings = array_map(
-            fn (array $round) => array_map(
-                function ($match) {
-                    shuffle($match);
-                    return $match;
-                },
-                $round
-            ),
-            $meetings
-        );
-
-        foreach ($meetings as $round => $matches) {
-
-            $matchday = [];
-
-            foreach ($matches as $index => $teams) {
-                [$teamA, $teamB] = $teams;
-
-                $pair = [];
-                $pair['host'] = $teamA;
-                $pair['visitor'] = $teamB;
-
-                $matchday[] = $pair;
-            }
-
-            $fixtures[] = $matchday;
-        }
-
-        print_r($fixtures);
-        die;
-
-        return $fixtures;
-    }
 }
 
-$teams = ['Arsenal', 'Burnley', 'Chelsea', 'Dinamo Moskwa'];
-$fixtures = new GenerateFixtureService($teams);
-$schedule = GenerateFixtureService::generateSchedule($fixtures);
+$teams = [
+    'Polska',
+    'Brazylia',
+    'Czechy',
+    'Grecja',
+    'Portugalia',
+    'Hiszpania',
+    'Francja',
+    'Włochy',
+    'Japonia',
+    'Dania',
+    'Szwecja',
+    'Kolumbia',
+];
 
-$params = [];
-$params['date'] = today()->format('Y-m-d');
-$params['hour'] = '20:30';
-$params['competitionID'] = 20;
-$params['stage'] = 1;
+$details = [
+    'competitionId' => 10,
+    'startDate'     => '2021-01-01',
+    'firstGameHour' => '20:00',
+    'lastGameHour'  => '22:00',
+    'gameDuration'  => 40,
 
-foreach ((new GenerateFixtureService($teams))->fixtures as $key => $matchday) {
-    $params['round'] = $key + 1;
-    foreach ($matchday as $team) {
-        $params['hosts'] = $team['host'];
-        $params['visitors'] = $team['visitor'];
-    }
+];
 
-    print_r($params);
-    /* DB::table('junior_fixtures')->insert($params); */
-};
+print_r(GenerateFixtureService::generateMeetingsWithSchedule($teams, $details));
