861d4b0edbf67468eb1c92596d488126f13ba457
Daniel <danielheinze96@gmail.com>
2021-08-14 22:09:50 +0200

    typo fix

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -24,56 +24,6 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        $juniorFixture =  JuniorFixture::where('competitionID', 30)->first();
-
-        /* There is no unplayed games. We can make promotions and relegations */
-        if ($juniorFixture->stage < 3) {
-            for ($group = 1; $group < 4; $group++) {
-                $teams = JuniorLeagueTable::where('competitionID', $juniorFixture->competitionID)
-                    ->where('stage', $juniorFixture->stage)
-                    ->where('group', $group)
-                    ->orderBy('points', 'DESC')->orderBy('bilans', 'DESC')->orderBy('goals_scored', 'DESC')
-                    ->get();
-
-                $teamToPromote = LeagueTableService::teamToPromote($teams);
-                $teamToRelegate = LeagueTableService::teamToReleagate($teams);
-
-                if ($group == 1) {
-                    if ($teamToRelegate !== null) {
-                        JuniorLeagueTable::where('teamId', $teamToRelegate->teamId)
-                            ->where('stage', $teamToRelegate->stage + 1)->where('group', $group)
-                            ->update(['group' => $group + 1]);
-                    }
-                    continue;
-                }
-
-                if ($group == 2) {
-                    if ($teamToRelegate !== null) {
-                        JuniorLeagueTable::where('teamId', $teamToRelegate->teamId)
-                            ->where('stage', $teamToRelegate->stage + 1)->where('group', $group)
-                            ->update(['group' => $group + 1]);
-                    }
-
-                    if ($teamToPromote !== null) {
-                        JuniorLeagueTable::where('teamId', $teamToPromote->teamId)
-                            ->where('stage', $teamToRelegate->stage + 1)->where('group', $group)
-                            ->update(['group' => $teamToPromote->group - 1]);
-                    }
-                    continue;
-                }
-
-                if ($group == 3) {
-                    if ($teamToPromote !== null) {
-                        JuniorLeagueTable::where('teamId', $teamToPromote->teamId)
-                            ->where('stage', $teamToPromote->stage + 1)->where('group', $group)
-                            ->update(['group' => $teamToPromote->group - 1]);
-                    }
-                    continue;
-                }
-            }
-        }
-
-
         return view('articles.index', [
             'allArticles'      => DB::table('articles')->orderBy('id', 'DESC')->limit(4)->get(),
             'leagueHeader'     => 'W JOMAFIVE',
